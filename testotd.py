d1a64e7c6e82bb8d9f03e51c9cafa6b0d17a26c9
Chris Maurer <chris.maurer@tradingtechnologies.com>
2018-06-08 15:03:31 +0900

    FIX Validation enhancements and fixed bug where rejected order was handled improperly

--- testotd.py
+++ testotd.py
@@ -2,12 +2,17 @@ __author__ = 'cmaurer'
 
 import argparse
 import os
+import copy
 
 # oc_log = r"/Users/cmaurer/testlog.log"
 
 for logfile in os.listdir(r"/var/log/debesys/"):
     if logfile.startswith("OC_") and logfile.endswith(".log"):
         oc_log = r"/var/log/debesys/" + logfile
+
+verification_dict = {}
+verify_data_list = []
+
 
 order_tags={"account_code": "PARTY_ROLE_ACCOUNT_CODE",
             "account": "PARTY_ROLE_CUSTOMER_ACCOUNT",
@@ -39,12 +44,16 @@ order_tags={"account_code": "PARTY_ROLE_ACCOUNT_CODE",
 fix_order_type = {"35=D": "NewOrderSingle",
                   "35=F": "OrderCancelRequest",
                   "35=G": "OrderCancelReplaceRequest",
-                  "35=8": "ExecutionReport"}
+                  "35=8": "ExecutionReport",
+                  "35=s": "NewOrderCross"}
 
 fix_tags = {"114": "PARTY_ROLE_DESK_ID",
             "115": "PARTY_ROLE_EXECUTING_FIRM",
+            "116": "PARTY_ROLE_ORDER_ENTRY_OPERATOR_ID",
+            "144": "PARTY_ROLE_DESK_ID",
             "439": "PARTY_ROLE_CLEARING_FIRM",  # Clearing Firm ID, NYBOT House Number, WCE House Number
             "440": "PARTY_ROLE_CLEARING_ACCOUNT",
+            "9121": "sMemo",
             "9195": "PARTY_ROLE_ACCOUNT_CODE",
             "9700": "order_origination",
             "9701": "order_capacity",
@@ -54,7 +63,10 @@ fix_tags = {"114": "PARTY_ROLE_DESK_ID",
             "9705": "PARTY_ROLE_EXECUTING_TRADER",
             "9706": "PARTY_ROLE_CLIENT_ID",
             "9707": "PARTY_ROLE_COMPOSITE_MIFID_ID",
-            "9208": "CTI Code"}
+            "9207": "account",
+            "9208": "CTI Code",
+            "sAuthorizedTrader": "sAuthorizedTrader"}
+            # "9207": "PARTY_ROLE_ORDER_ENTRY_OPERATOR_ID",
 
 
 def optmenu():
@@ -68,7 +80,7 @@ def optmenu():
 
 def parse_log_message(log_data):
 
-    should_not_validate = ['client_time_sent', 'connection_id']
+    should_not_validate = ['client_time_sent', 'connection_id', 'account_id']
     log_message_dict = {}
     log_msg_type = log_data[0]
     logmsg = log_data[1:]
@@ -158,6 +170,12 @@ def parse_fix_message(line):
     log_message_dict = {}
     if "=" not in line[-1]:
         line.pop(-1)
+    # if "35=s" in line:
+    #     if "54=1" in line:
+    #         log_msg_type = "FIX_" + fix_order_type[line[2]] + "-BUY"
+    #     else:
+    #         log_msg_type = "FIX_" + fix_order_type[line[2]] + "-SELL"
+    # else:
     log_msg_type = "FIX_" + fix_order_type[line[2]]
     # log_message_dict = {k: v for k, v in (x.split('=') for x in line)}  # Not compatible with Python 2.6
     for x in line[3:]:
@@ -183,6 +201,9 @@ def parse_logfile_line(line):
             log_message.append(line_list[2])
             log_message.append(line_list[3])
             log_message.extend(line_list[4].split(" "))
+        elif "TradeCaptureReportAck" in line:
+            log_message = [line_list[0], line_list[1].lstrip(' ')]
+            log_message.extend(line_list[2].split(" "))
         elif "TradeCaptureReport" in line:
             log_message = [line_list[0], line_list[1].lstrip(' ')]
             log_message.append(line_list[2])
@@ -197,110 +218,141 @@ def parse_logfile_line(line):
     return log_message
 
 
-def verify_otd_data(order_id):
+def validate_messages(line, verbose=False, validate_fix=False):
 
-    verbose = True
-    validate_fix = False
-    verification_dict = {}
-    verify_data_list = []
+    fix = False
     er_counter = 0
     cr_counter = 0
     fixer_counter = 0
     fixcr_counter = 0
-    get_fix_nos = False
+
+    verification_list = []
+    line = line.rstrip(r"\n")
+    log_message = parse_logfile_line(line)
+    if "8=FIX" in str(log_message):
+        if not validate_fix:
+            pass
+        else:
+            # if "35=s" in log_message:
+            #     buy_sell_split = log_message.index("54=2")
+            #     buy_log_message = log_message[:buy_sell_split]
+            #     sell_log_message = buy_log_message[0:19]
+            #     sell_log_message.extend(log_message[buy_sell_split:])
+            #     log_msg_type, log_message_dict, exec_type = parse_fix_message(buy_log_message)
+            #     log_msg_type, log_message_dict, exec_type = parse_fix_message(sell_log_message)
+            #     print "buy_log_message:", buy_log_message
+            #     print "sell_log_message:", sell_log_message
+            # else:
+            fix = True
+            log_msg_type, log_message_dict, exec_type = parse_fix_message(log_message)
+    else:
+        if "secondary_order_id" in line:
+            exch_order_num = ((line.split("secondary_order_id=")[0]).split("=")[-1]).rstrip(" | ")
+            exch_order_num = None if exch_order_num == '' else exch_order_num
+            order_id.append(exch_order_num)
+        log_msg_type, log_message_dict, exec_type = parse_log_message(log_message)
+
+    # Prepare log messages for validation
+    if "ExecutionReport" in log_msg_type:
+        if fix:
+            fixer_counter += 1
+            log_message_type = '-'.join([log_msg_type, str(fixer_counter)])
+        else:
+            er_counter += 1
+            log_message_type = '-'.join([log_msg_type, str(er_counter)])
+    elif "OrderCancelReplaceRequest" in log_msg_type:
+        if fix:
+            fixcr_counter += 1
+            log_message_type = '-'.join([log_msg_type, str(fixcr_counter)])
+        else:
+            cr_counter += 1
+            log_message_type = '-'.join([log_msg_type, str(cr_counter)])
+    else:
+        log_message_type = log_msg_type
+
+    print "\n" + "#" * 20
+    print log_message_type, "-", exec_type
+    print "#" * 20 + "\n"
+
+    if fix:
+        # Translate FIX Tags into TTUS Order Tags
+        for k, v in log_message_dict.iteritems():
+            if k in fix_tags.keys():
+                log_message_dict[fix_tags[k]] = log_message_dict.pop(k)
+
+        # Translate booleans to human readable
+        for k, v in log_message_dict.iteritems():
+            if "ORDER_ATTRIBUTE" in k:
+                if log_message_dict[k] == "0":
+                    log_message_dict[k] = "No"
+                if log_message_dict[k] == "1":
+                    log_message_dict[k] = "Yes"
+            elif "order_capacity" in k:
+                if log_message_dict[k] == "0":
+                    log_message_dict[k] = "ORDER_CAPACITY_DEAL"
+                if log_message_dict[k] == "1":
+                    log_message_dict[k] = "ORDER_CAPACITY_MATCH"
+                if log_message_dict[k] == "2":
+                    log_message_dict[k] = "ORDER_CAPACITY_AGENCY"
+            elif "order_origination" in k:
+                if log_message_dict[k] == "0":
+                    log_message_dict[k] = "ORDER_ORIGINATION_OTHER_NON_DEA"
+                if log_message_dict[k] == "1":
+                    log_message_dict[k] = "ORDER_ORIGINATION_OTHER_NON_DEA"
+            elif "PARTY_ROLE_DESK_ID" in k:
+                log_message_dict[k] = log_message_dict[k].split("|")[-1]
+
+        if "PARTY_ROLE_ORDER_ENTRY_OPERATOR_ID" in log_message_dict:
+            log_message_dict["sAuthorizedTrader"] = log_message_dict["PARTY_ROLE_ORDER_ENTRY_OPERATOR_ID"].split("|")[-1]
+
+
+        for k, v in log_message_dict.iteritems():
+            if k in fix_tags.values():
+                element = (k, v)
+                print element
+                verification_list.append(element)
+    else:
+        for element in log_message_dict.iteritems():
+            for k, v in order_tags.iteritems():
+                if k in str(element) or v in str(element):
+                    print element
+                    verification_list.append(element)
+                    break
+
+    if verbose:
+        if fix:
+            print "\n\nLINE:", " ".join(line.encode("ascii").split("\x01"))
+        else:
+            print "\n\nLINE:", line
+
+    verification_list.sort()
+    verification_dict[log_message_type] = verification_list
+    verify_data_list.append([log_message_type, exec_type])
+
+
+def verify_otd_data(order_id):
+
+    verbose = True
+    validate_fix = True
+
+    fix_nos = None
     found_fix_nos = False
 
     logfile = open(oc_log, 'r')
     for line in logfile.readlines():
         try:
-            start_verification = False
-            fix = False
-            if get_fix_nos:
-                if all(fix_nos_element in line for fix_nos_element in ["Send:", "8=FIX"]):
-                    start_verification = True
+            # Ridiculous hack to catch lines that are not Order updates
+            if len(line.split(" | ")[5].split(" ")) > 1:
+                continue
+            if all(fix_nos_element in line for fix_nos_element in ["Send:", "8=FIX"]):
+                if any(fix_nos_element in line for fix_nos_element in ["35=D", "35=s"]):
+                    fix_nos = copy.copy(line)
+            if any(ordid in line for ordid in order_id):
+                if not found_fix_nos and fix_nos is not None:
+                    # print "fix_nos:\n\n", fix_nos
+                    validate_messages(fix_nos, verbose, validate_fix)
                     found_fix_nos = True
-
-            if any(ordid in line for ordid in order_id) and "om_order_responder_inl.h" not in line and not \
-                    (line.split(" | ")[5]).startswith(" sender_sub_id="):
-                start_verification = True
-                get_fix_nos = True
-
-            if start_verification:
-                # Ridiculous hack to catch lines that are not Order updates
-                if len(line.split(" | ")[5].split(" ")) > 1:
-                    continue
-                verification_list = []
-                line = line.rstrip(r"\n")
-                log_message = parse_logfile_line(line)
-                if "8=FIX" in str(log_message):
-                    if not validate_fix:
-                        continue
-                    fix = True
-                    log_msg_type, log_message_dict, exec_type = parse_fix_message(log_message)
-                else:
-                    if "secondary_order_id" in line:
-                        exch_order_num = ((line.split("secondary_order_id=")[0]).split("=")[-1]).rstrip(" | ")
-                        order_id.append(exch_order_num)
-                    log_msg_type, log_message_dict, exec_type = parse_log_message(log_message)
-
-                # Prepare log messages for validation
-                if "ExecutionReport" in log_msg_type:
-                    if fix:
-                        fixer_counter += 1
-                        log_message_type = '-'.join([log_msg_type, str(fixer_counter)])
-                    else:
-                        er_counter += 1
-                        log_message_type = '-'.join([log_msg_type, str(er_counter)])
-                elif "OrderCancelReplaceRequest" in log_msg_type:
-                    if fix:
-                        fixcr_counter += 1
-                        log_message_type = '-'.join([log_msg_type, str(fixcr_counter)])
-                    else:
-                        cr_counter += 1
-                        log_message_type = '-'.join([log_msg_type, str(cr_counter)])
-                else:
-                    log_message_type = log_msg_type
-
-                print "\n" + "#" * 20
-                print log_message_type, "-", exec_type
-                print "#" * 20 + "\n"
-
-                if fix:
-                    # Translate FIX Tags into TTUS Order Tags
-                    for k, v in log_message_dict.iteritems():
-                        if k in fix_tags.keys():
-                            log_message_dict[fix_tags[k]] = log_message_dict.pop(k)
-
-                    # Translate booleans to human readable
-                    for k, v in log_message_dict.iteritems():
-                        if "ORDER_ATTRIBUTE" in k:
-                            if log_message_dict[k] == "0":
-                                log_message_dict[k] = "No"
-                            if log_message_dict[k] == "1":
-                                log_message_dict[k] = "Yes"
-
-                    for k, v in log_message_dict.iteritems():
-                        if k in fix_tags.values():
-                            element = (k, v)
-                            print element
-                            verification_list.append(element)
-                else:
-                    for element in log_message_dict.iteritems():
-                        for k, v in order_tags.iteritems():
-                            if k in str(element) or v in str(element):
-                                print element
-                                verification_list.append(element)
-                                break
-
-                if verbose:
-                    print "\n\nLINE:", line
-
-                verification_list.sort()
-                verification_dict[log_message_type] = verification_list
-                verify_data_list.append([log_message_type, exec_type])
-
-                if found_fix_nos:
-                    get_fix_nos = False
+                validate_messages(line, verbose, validate_fix)
 
         except:
             continue
@@ -350,6 +402,9 @@ def verify_otd_data(order_id):
             else:
                 index = 1
                 while True:
+                    if any("FIX_ExecutionReport" in validation_msg for validation_msg in [verify_data_list[i][1], verify_data_list[i+index][1]])\
+                            and not all("FIX_" in validation_msg for validation_msg in [verify_data_list[i][1], verify_data_list[i+index][1]]):
+                        break
                     print "{0}\n{1} and {2} match: {3}\n".format(
                         "-"*56, verify_data_list[i][1], verify_data_list[i+index][1],
                         verification_dict[verify_data_list[i][0]] == verification_dict[verify_data_list[i+index][0]])
@@ -370,6 +425,8 @@ def verify_otd_data(order_id):
                     else:
                         index += 1
 
+    logfile.close()
+
 order_id = optmenu()
-# order_id = ["12f8d8d8-bf46-4a99-9f1e-39f36a14cfa4", ]
+# order_id = ["c10f8b2d-068a-4d18-ad9f-741b8078858a", ]
 verify_otd_data(order_id)
